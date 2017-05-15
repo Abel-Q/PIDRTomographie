b75d552387961bee7eab0d3c0294ad66c19c2756
Abel-Q <abel.querou@telecomnancy.net>
2017-05-15 10:17:49 +0200

    test encore ...

--- filtre.c
+++ filtre.c
@@ -5,16 +5,17 @@
 
 int main(void)
 {
-double array[] = {1, 2, 3, 4, 5, 6, 7, 8};
-//double array2[] = {1, 6, 1, 4, 5, 0, 8, 7, 8, 6, 1,0};
+double array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+double array2[] = {20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
 double *out;
 double *err;
-int i,size = 8;
+int i,size = 10;
 
-fftw_complex *out_cpx, *in_cpx, *result_cpx;
+fftw_complex *out_cpx, *in_cpx, *result_cpx, *out_cpx2;
 
 in_cpx = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*size);
 out_cpx = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*size);
+out_cpx2 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*size);
 result_cpx = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*size);
 fftw_plan fft;
 fftw_plan ifft;
@@ -28,18 +29,20 @@ for (i=0; i<size; i++){
 }
 
 fft = fftw_plan_dft_1d(size, in_cpx, out_cpx,FFTW_FORWARD, FFTW_ESTIMATE);  //Setup fftw plan for fft
+fftw_execute(fft);
+for(i=0;i<size; i++){
+  out_cpx[i][0]=out_cpx[i][0]*array2[i];
+}
+
 ifft = fftw_plan_dft_1d(size, out_cpx, result_cpx, FFTW_BACKWARD, FFTW_ESTIMATE);   //Setup fftw plan for ifft
 
-fftw_execute(fft);
 fftw_execute(ifft);
 
 //printf("Input:    \tOutput:    \tError:\n");
-printf("Input:    \tOutput:\n");
+printf("Input:    \tOutput:      \t Result :\n");
 for(i=0;i<size;i++)
 {
-//err[i] = abs(array[i] - out[i]);
-printf("%f\t%f+i%f\n",(array[i]),out_cpx[i][0],out_cpx[i][1]);
-//printf("%f\t%f\t%f\n",(array[i]),out[i],err[i]);
+printf("%f\t%f+i%f\t%f+i%f\n",(array[i]),out_cpx[i][0],out_cpx[i][1],result_cpx[i][0],result_cpx[i][1]);
 }
 
 
